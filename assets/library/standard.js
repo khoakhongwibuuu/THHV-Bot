bc6a1c2349cd0d123714961c285ed1e5dd76011f
khoakhongwibuuu <anhkhoa.vv@gmail.com>
2024-08-28 22:24:45 +0700

    lib update

--- assets/library/standard.js
+++ assets/library/standard.js
@@ -3,6 +3,33 @@ const path = require('node:path');
 
 const dirname = global.dirname;
 
+const trueRnd = (l, h) => {
+	return l + Math.floor(Math.random() * (h - l + 1));
+}
+
+const shuffle = (array) => {
+	for (let idx = 0; idx < (array.length) * (array.length); idx++) {
+		let i = trueRnd(0, array.length - 1);
+		let j = trueRnd(0, array.length - 1);
+		[array[i], array[j]] = [array[j], array[i]];
+	}
+}
+
+const randomEvent = (offset) => {
+	// 1 in offset cases returns true
+	return trueRnd(1, offset) === trueRnd(1, offset);
+}
+
+const randomPercent = (offset) => {
+	// offset% cases returns true
+	return Math.random() * 100 < offset;
+}
+
+module.exports.trueRnd = trueRnd;
+module.exports.shuffle = shuffle;
+module.exports.randomEvent = randomEvent;
+module.exports.randomPercent = randomPercent;
+
 String.prototype.logToFile = function () {
 	fs.appendFile(dirname + '/logs/' + global.BotStartTime.replace(/:/g, "") + '.log', this + '\n', (err) => {
 		if (err) throw err;
@@ -13,6 +40,10 @@ String.prototype.logOffline = function () {
 	const content = JSON.stringify(this)
 	console.log(content.substring(1, content.length - 1));
 	content.substring(1, content.length - 1).logToFile();
+}
+
+String.prototype.URLdecode = function () {
+	return decodeURIComponent(this);
 }
 
 String.prototype.hasWhiteSpace = function () {
@@ -31,12 +62,32 @@ String.prototype.firstDigit = function () {
 	return this[0];
 }
 
-Array.prototype.lastValue = function() {
+Array.prototype.argList = function (mode) {
+	let res = "";
+	this.forEach((e, i, a) => {
+		if (mode === "mention")
+			res += `<@${e}>`;
+		else if (mode === "role-mention")
+			res += `<@&${e}>`;
+		else if (mode === "shaded")
+			res += `\`${e}\``;
+		else res += e;
+		if (i < a.length - 1)
+			res += ', '
+	});
+	return res;
+}
+
+Array.prototype.lastValue = function () {
 	return this[this.length - 1];
 }
 
-Array.prototype.firstValue = function() {
+Array.prototype.firstValue = function () {
 	return this[0];
+}
+
+Array.prototype.randomValue = function () {
+	return this[trueRnd(0, this.length - 1)];
 }
 
 const clearCache = (sessionId) => {
