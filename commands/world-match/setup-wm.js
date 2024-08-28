b3c869008b86e4b792b476557feaea6b7cc3f8f0
khoakhongwibuuu <anhkhoa.vv@gmail.com>
2024-08-28 14:52:40 +0700

    fix

--- commands/world-match/setup-wm.js
+++ commands/world-match/setup-wm.js
@@ -14,7 +14,7 @@ const wordLib = require(path.join(dirname, 'modules/word-match/lib/wordLib.js'))
 module.exports = {
     data: new Discord.SlashCommandBuilder()
         .setName('wm-setup')
-        .setDescription('[Moderators Only] - Set wWrdMatch game at this channel.')
+        .setDescription('[Moderators Only] - Set WordMatch game at this channel.')
     ,
     async execute(interaction) {
         if (discordAPI.isModerator(interaction.guild.id, interaction.user.id)) {
