21f43d246feb462846d0ea2a0483371b3d3b8220
Khoa Vu <anhkhoa.vv@gmail.com>
2024-12-16 20:06:45 +0700

    drafting new command

--- commands/codeforces-utils/handle.js
+++ commands/codeforces-utils/handle.js
@@ -21,11 +21,38 @@ module.exports = {
         )
     ,
     async execute(interaction) {
-        await interaction.deferReply({ ephemeral: true });
-        await interaction.editReply({ content: 'This command is under development!', ephemeral: true });
-        return;
+        await interaction.deferReply({ ephemeral: false });
         const handle = interaction.options.getString("handle");
-        const statusData = await codeforcesLib.fetchData(`https://codeforces.com/api/user.status?handle=${handle}`);
         const infoData = await codeforcesLib.fetchData(`https://codeforces.com/api/user.info?handles=${handle}`);
+        if (!infoData) {
+            await interaction.editReply({ content: 'No user with given handle is found.', ephemeral: false });
+            return;
+        }
+
+        const rank = infoData[0].rank ?? "Unknown";
+        const maxRank = infoData[0].maxRank ?? "Unknown";
+
+        const rating = infoData[0].rating ?? 0;
+        const maxRating = infoData[0].maxRating ?? 0;
+
+        const sentEmbed = new Discord.EmbedBuilder()
+            .setColor(0x0099FF)
+            .setTitle(handle)
+            .setURL(`https://codeforces.com/profile/${handle}`)
+            .setThumbnail(infoData[0].avatar)
+            .addFields(
+                { name: 'Rank', value: rank, inline: true },
+                { name: 'Max rank', value: maxRank, inline: true },
+                { name: '\u200B', value: '\u200B' },
+                { name: 'Rating', value: rating.toString(), inline: true },
+                { name: 'Max rating', value: maxRating.toString(), inline: true },
+                { name: '\u200B', value: '\u200B' },
+            )
+            .setTimestamp()
+
+        if (infoData[0].hasOwnProperty("firstName") && infoData[0].hasOwnProperty("lastName"))
+            sentEmbed.setDescription(`Info of ${infoData[0].firstName} ${infoData[0].lastName}`)
+
+        await interaction.editReply({ embeds: [sentEmbed], ephemeral: false });
     },
 };
