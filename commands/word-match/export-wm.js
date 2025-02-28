d7e63c335925b5b33678c8a107343c6bdbca41ba
khoakhongwibuuu <anhkhoa.vv@gmail.com>
2025-02-28 22:49:13 +0700

    Fix export modules

--- commands/word-match/export-wm.js
+++ commands/word-match/export-wm.js
@@ -4,12 +4,12 @@ const path = require('path');
 const Discord = require('discord.js');
 
 // Module Specified
-const mcLib = require(path.join(global.dirname, 'modules/multiple-choice/lib/gameLib.js'));
+const wordLib = require(path.join(global.dirname, 'modules/word-match/lib/wordLib.js'));
 
 module.exports = {
     data: new Discord.SlashCommandBuilder()
-        .setName('mc-export')
-        .setDescription('[Moderators Only] - Export this server trivia-game data.')
+        .setName('wm-export')
+        .setDescription('[Moderators Only] - Export this server word-match data.')
         .setDMPermission(false)
     ,
     async execute(interaction) {
@@ -17,19 +17,15 @@ module.exports = {
             interaction.reply({ content: "🚫 Bạn không có quyền sử dụng lệnh này.", ephemeral: true });
             return;
         }
-        if (!mcLib.isSetup(interaction.guild.id)) {
+        if (!wordLib.isSetup(interaction.guild.id)) {
             interaction.reply({ content: "⚠️ Không tìm thấy dữ liệu của server này.", ephemeral: true });
             return;
         }
-        if (interaction.channel.id !== mcLib.getRoomId(interaction.guild.id)) {
-            interaction.reply({ content: `⚠️ Vui lòng sử dụng lệnh tại phòng chơi <#${mcLib.getRoomId(interaction.guild.id)}>`, ephemeral: true });
+        if (interaction.channel.id !== wordLib.getRoomId(interaction.guild.id)) {
+            interaction.reply({ content: `⚠️ Vui lòng sử dụng lệnh tại phòng chơi <#${wordLib.getRoomId(interaction.guild.id)}>`, ephemeral: true });
             return;
         }
-        if (mcLib.isRunning(interaction.guild.id)) {
-            interaction.reply({ content: '⚠️ Bạn không được phép sử dụng lệnh này khi có lượt chơi đang diễn ra. Vui lòng chờ lượt chơi đó hoàn tất.', ephemeral: true });
-            return;
-        }
-        const dat = mcLib.loadRawGuildFile(interaction.guild.id);
+        const dat = wordLib.loadRawGuildFile(interaction.guild.id);
         const sentText = null;
         interaction.reply({
             embeds: [new Discord.EmbedBuilder()
