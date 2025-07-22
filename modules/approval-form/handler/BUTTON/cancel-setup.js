ee83a4a74963198d7976ad266e7f4745d7dce598
khoakhongwibuuu <anhkhoa.vv@gmail.com>
2025-07-22 13:58:33 +0700

    Fix moderation-side error handling logic

--- modules/approval-form/handler/BUTTON/cancel-setup.js
+++ modules/approval-form/handler/BUTTON/cancel-setup.js
@@ -1,6 +1,18 @@
-const { memory } = global.customLib;
-
 module.exports.exec = async (interaction, UUID) => {
-    memory.deleteData(UUID);
-    interaction.message.delete();
+    try {
+        const channelId = interaction.channelId;
+        const messageId = interaction.message.id;
+
+        const channel = await interaction.client.channels.fetch(channelId);
+        if (!channel) return;
+
+        const message = await channel.messages.fetch(messageId);
+        await message.delete();
+    } catch (err) {
+        console.error(err);
+        await interaction.reply({
+            ephemeral: true,
+            content: `Đã có lỗi xảy ra.`
+        });
+    }
 }
