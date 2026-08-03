034a5818a152cbb2d992e962c1a028ede3cd5f2f
khoakhongwibuuu <anhkhoa.vv@gmail.com>
2026-08-03 15:42:56 +0700

    cleanup

--- assets/api-template.js
+++
@@ -1,9 +0,0 @@
-        const isAdmin = await discordAPIv2.isAdmin(interaction.guild.id, interaction.user.id);
-        // if (!discordAPI.isAdmin(interaction.guild.id, interaction.user.id)) {
-        if (!isAdmin) {
-
-        const isMod = await discordAPIv2.isModerator(interaction.guild.id, interaction.user.id);
-        // if (!discordAPI.isModerator(interaction.guild.id, interaction.user.id)) {
-        if (!isMod) {
-
-        }}
