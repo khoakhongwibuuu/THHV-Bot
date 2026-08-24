27f7a1856e9eb0c3e9dcdda58d674baa98f59692
khoakhongwibuuu <anhkhoa.vv@gmail.com>
2026-08-24 17:02:43 +0700

    refactor: simplify offline-loader modules by removing unused file system operations

--- modules/codeforces-utils/offline-loader.js
+++ modules/codeforces-utils/offline-loader.js
@@ -2,5 +2,4 @@
 const path = require('node:path');
 const { dirname } = global.variable;
 
-const codeforcesLib = require(path.join(dirname, '/modules/codeforces-utils/lib/codeforcesLib.js'));
-global.customLib.codeforcesLib = codeforcesLib;
+global.customLib.codeforcesLib = require(path.join(dirname, '/modules/codeforces-utils/lib/codeforcesLib.js'));
--- modules/ticket/offline-loader.js
+++ modules/ticket/offline-loader.js
@@ -1,19 +1,5 @@
 // Packages
-const fs = require('node:fs');
 const path = require('node:path');
 const { dirname } = global.variable;
 
-const ticketLib = require(path.join(dirname, '/modules/ticket/lib/ticketLib.js'));
-global.customLib.ticketLib = ticketLib;
-
-const configPath = path.join(dirname, 'configs/ticket/config');
-if (!fs.existsSync(configPath)) {
-    fs.mkdirSync(configPath, { recursive: true });
-}
-
-const guildFiles = fs.readdirSync(configPath).filter(file => file.endsWith('json'));
-
-for (const guildFile of guildFiles) {
-    const guildId = guildFile.slice(0, guildFile.lastIndexOf('.')) || guildFile;
-    ticketLib.preLoad(guildId);
-}
+global.customLib.ticketLib = require(path.join(dirname, '/modules/ticket/lib/ticketLib.js'));
