403e33bb845224b689a7bd889a8bf3fc1a519e97
khoakhongwibuuu <anhkhoa.vv@gmail.com>
2026-09-02 23:00:08 +0700

    refactor: enhance scoreboard display with ordinal indicators for player rankings

--- commands/trivia-game/scoreboard-mc.js
+++ commands/trivia-game/scoreboard-mc.js
@@ -1,5 +1,6 @@
 // Packages
 const Discord = require('discord.js');
+const stdLib = require('#assets/library/standard.js');
 const gameLib = require('#modules/trivia-game/lib/gameLib.js');
 
 module.exports = {
@@ -18,26 +19,43 @@ module.exports = {
             await interaction.reply({ content: "🔍 Không tìm thấy dữ liệu của server này.", ephemeral: true });
             return;
         }
+
         const numOfEntries = interaction.options.getInteger('number-of-players') ?? 5;
         if (numOfEntries < 1 || numOfEntries > 20) {
             await interaction.reply({ content: "⚠️ Số lượng người chơi tối thiểu là 1 và tối đa là 20.", ephemeral: true });
             return;
         }
+
         let rawmap = new Map();
         const playerdata = await gameLib.getGuildConfig(interaction.guild.id);
         Object.keys(playerdata.playerdata).forEach(key => rawmap.set(key, playerdata.playerdata[key].score.lastValue()));
         const sortedEntries = Array.from(rawmap.entries()).sort((a, b) => b[1] - a[1]);
+
         if (sortedEntries.length === 0) {
             await interaction.reply({ content: "🔍 Chưa có người chơi nào ghi điểm.", ephemeral: true });
             return;
         }
+
         const lim = Math.min(sortedEntries.length, numOfEntries);
         const topList = new Map(sortedEntries.slice(0, lim));
+
         let content = ``;
         const sentEmbed = new Discord.EmbedBuilder();
         sentEmbed.setTitle(`Danh sách ${lim} người chơi có điểm Trivia Game cao nhất server.`);
         sentEmbed.setFooter({ text: `Đang hiển thị ${lim} trong tổng số ${sortedEntries.length} người chơi đã ghi điểm.` });
-        topList.forEach((v, k) => content += `* <@${k}> : \`${v} điểm\`.\n`);
+
+        let index = 0;
+        topList.forEach((v, k) => {
+            index++;
+            content += `* ${
+                index === 1 ? ':first_place:'
+                : index === 2 ? ':second_place:'
+                : index === 3 ? ':third_place:'
+                : ''} ${
+                    index > 3 ? `**${stdLib.ordinal(index)}**`
+                    : ''} <@${k}>: \`${v} điểm\`.\n`
+        });
+
         sentEmbed.setDescription(content);
         await interaction.reply({ embeds: [sentEmbed], ephemeral: !await gameLib.isInRoom(interaction.guild.id, interaction.channel.id) | await gameLib.isRunning(interaction.guild.id) });
     },
